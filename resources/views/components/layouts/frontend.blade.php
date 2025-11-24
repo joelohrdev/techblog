df60df686fb6dfc4588530ae7f81a7b7eff2fcc0
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-24 13:29:46 -0600

    Added favicon

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -26,7 +26,7 @@
     @endif
 
     <link rel="icon" href="/favicon.ico" sizes="any">
-    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/logo.svg" type="image/svg+xml">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <link rel="preconnect" href="https://fonts.bunny.net">
