e428653b5bf8d4e9f36e446b8ca55ef5b5bc48e6
jlarm <117611704+jlarm@users.noreply.github.com>
2025-11-24 14:44:21 -0600

    Removed index cache

--- app/Livewire/Post/Index.php
+++ app/Livewire/Post/Index.php
@@ -1,22 +1,23 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Post;
 
 use App\Models\Post;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Livewire\WithPagination;
 
-class Index extends Component
+final class Index extends Component
 {
     use WithPagination;
 
     #[Computed]
     public function posts()
     {
-        return Cache::remember('posts', now()->addDay(5), fn () => Post::query()->paginate(10));
+        return Post::query()->latest()->paginate(10);
     }
 
     public function render(): View
